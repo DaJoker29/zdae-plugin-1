28d5aaa5cfe3e705ef07790cf39e1dc88f57bb51
Dewitt Buckingham <dj729@comcast.net>
2019-03-16 14:57:21 -0700

    chore: fix function call

--- zdae-plugin.php
+++ zdae-plugin.php
@@ -8,10 +8,10 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 function replicator_handler() {
   $alias = $_GET['alias'];
   echo '<div>';
-  echo 'Your current alias is <strong>$alias<strong>';
+  echo 'Your current alias is <strong>' + $alias + '<strong>';
   echo '</div>';
 
-  echo '<form action="' + $admin_url( 'admin-post.php') + '" method="post">';
+  echo '<form action="' + admin_url( 'admin-post.php') + '" method="post">';
   echo '<input type="hidden" name="alias" value="' + $alias + '" />';
   echo '<input type="submit" value="Create Website" />';
   // echo '<button>Create Website</button>'
