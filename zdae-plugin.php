28fde9865f417b27baafcddd2bee3292d5ea02b7
Dewitt Buckingham <dj729@comcast.net>
2019-03-16 14:32:35 -0700

    chore: add changes

--- zdae-plugin.php
+++ zdae-plugin.php
@@ -7,7 +7,7 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 function replicator_handler() {
   $alias = get_query_var( 'alias' );
-  return echo $alias;
+  return $alias;
 }
 
 add_shortcode( 'replicator', 'replicator_handler' );
