bfa702c05f57492aaf11cc9b9a92b4bd167e4b53
Dewitt Buckingham <dj729@comcast.net>
2019-03-16 16:51:54 -0700

    fix: cleanup

--- zdae-plugin.php
+++ zdae-plugin.php
@@ -7,6 +7,9 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 function replicator_handler() {
   $alias = $_GET['alias'];
+
+  // TODO: Display a different form if the ALIAS field isn't provided.
+
   echo '<div>';
   echo 'Your current alias is <strong>'.$alias.'</strong>';
   echo '</div>';
@@ -15,30 +18,28 @@ function replicator_handler() {
   echo '<input type="hidden" name="action" value="zdae_replicator_action" />';
   echo '<input type="hidden" name="alias" value="'.$alias.'" />';
   echo '<input type="submit" value="Create Website" />';
-  // echo '<button>Create Website</button>'
   echo '</form>';
 }
-
 add_shortcode( 'replicator', 'replicator_handler' );
-
-add_action( 'admin_post_zdae_replicator_action', 'replicate' );
 
 function replicate() {
   status_header(200);
   $alias = $_POST['alias'];
-  $blog_details = get_blog_details( get_current_blog_id );
+  $blog_details = get_blog_details( get_current_blog_id() );
 
-  $domain = $blog_details->domain;
+  $domain = $blog_details->domain; // TODO: Verify this is working.
   $path = $alias;
   $title = $alias;
-  $user_id = 1;
+  $user_id = 1; // FIXME: fetch super admin id and user that instead.
 
 
   wpmu_create_blog( $domain, $path, $title, $user_id );
 
   // TODO: Check if blog creation was successful. If so, redirect there. Otherwise, handle error.
+
   wp_redirect( get_site_url( get_current_blog_id(), $alias) );
   exit();
 }
+add_action( 'admin_post_zdae_replicator_action', 'replicate' );
 
  ?>
