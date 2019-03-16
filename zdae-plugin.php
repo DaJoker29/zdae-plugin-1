1b3c1beab195a6ce404f0de1f859ab929f413acc
Dewitt Buckingham <dj729@comcast.net>
2019-03-16 14:53:58 -0700

    chore: add form

--- zdae-plugin.php
+++ zdae-plugin.php
@@ -7,7 +7,15 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 function replicator_handler() {
   $alias = $_GET['alias'];
-  return "Your current alias is <strong>$alias<strong>";
+  echo '<div>';
+  echo 'Your current alias is <strong>$alias<strong>';
+  echo '</div>';
+
+  echo '<form action="$admin_url( 'admin-post.php')" method="post">'
+  echo '<input type="hidden" name="alias" value="$alias" />';
+  echo '<input type="submit" value="Create Website" />';
+  // echo '<button>Create Website</button>'
+  echo '</form>';
 }
 
 add_shortcode( 'replicator', 'replicator_handler' );
