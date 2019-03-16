7af59580aff1e04a778aed36e7e4cf6552c137e2
Dewitt Buckingham <dj729@comcast.net>
2019-03-16 14:37:24 -0700

    chore: add strong tags

--- zdae-plugin.php
+++ zdae-plugin.php
@@ -7,7 +7,7 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 function replicator_handler() {
   $alias = $_GET['alias'];
-  return "alias is $alias";
+  return "Your current alias is <strong>$alias<strong>";
 }
 
 add_shortcode( 'replicator', 'replicator_handler' );
